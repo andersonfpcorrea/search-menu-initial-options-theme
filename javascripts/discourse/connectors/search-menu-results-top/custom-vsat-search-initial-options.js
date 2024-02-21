bccccd2ad3d101abe38686ebf5673b1bf325e1f1
Anderson Correa <andersonfpcorrea@gmail.com>
2024-02-20 21:47:50 -0300

    feat: add content

--- javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
+++ javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
@@ -7,18 +7,20 @@ export default class CustomVsatSearchInitialOptions extends Component {
     const inputSearch = document.querySelector("#search-term");
     console.log({ inputSearch });
     inputSearch.addEventListener("change", (e) => {
-      console.log(e);
-      const el = document.querySelector("#custom-vsat-init-options");
-      console.log({ el });
-      const labelEl = el?.querySelector("span.label-suffix");
-      console.log({ labelEl });
-      if (labelEl) labelEl.innerText = "in Viasat Help Articles";
-      el?.addEventListener("click", (e) => {
-        console.log({ e });
-        const query = el?.querySelector("span.search-item-prefix").innerText;
-        console.log({ query });
-        location.href = `https://www.forum.test.viasat.com/docs?search=${query}&tag=article`;
-      });
+      console.log({ e });
+      if (e.target.value.length > 2) {
+        const el = document.querySelector("#custom-vsat-init-options");
+        console.log({ el });
+        const labelEl = el?.querySelector("span.label-suffix");
+        console.log({ labelEl });
+        if (labelEl) labelEl.innerText = "in Viasat Help Articles";
+        el?.addEventListener("click", (e) => {
+          console.log({ e });
+          const query = el?.querySelector("span.search-item-prefix").innerText;
+          console.log({ query });
+          location.href = `https://www.forum.test.viasat.com/docs?search=${query}&tag=article`;
+        });
+      }
     });
   }
 }
