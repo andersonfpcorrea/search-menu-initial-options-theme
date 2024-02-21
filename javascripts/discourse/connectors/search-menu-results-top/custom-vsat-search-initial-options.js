bb60dd60f3b41b468b42329b78b208c7a6b62551
Anderson Correa <andersonfpcorrea@gmail.com>
2024-02-20 21:34:22 -0300

    feat: add content

--- javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
+++ javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
@@ -3,13 +3,15 @@ import Component from "@glimmer/component";
 export default class CustomVsatSearchInitialOptions extends Component {
   constructor() {
     super();
-    console.log("Hello, world");
-    const el = document.querySelector("#custom-vsat-init-options");
-    const labelEl = el?.querySelector("span.label-suffix");
-    labelEl?.innerText = "in Viasat Help Articles";
-    el?.addEventListener("click", () => {
-      const query = el?.querySelector("span.search-item-prefix").innerText;
-      location.href = `https://www.forum.test.viasat.com/docs?search=${query}&tag=article`;
+    const inputSearch = document.querySelector("#search-term");
+    inputSearch.addEventListener("input", () => {
+      const el = document.querySelector("#custom-vsat-init-options");
+      const labelEl = el?.querySelector("span.label-suffix");
+      if (labelEl) labelEl.innerText = "in Viasat Help Articles";
+      el?.addEventListener("click", () => {
+        const query = el?.querySelector("span.search-item-prefix").innerText;
+        location.href = `https://www.forum.test.viasat.com/docs?search=${query}&tag=article`;
+      });
     });
   }
 }
