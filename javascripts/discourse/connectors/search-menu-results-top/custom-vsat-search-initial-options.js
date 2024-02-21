702b7bff56f5b96b8b9ec9139d7158151d6417ac
Anderson Correa <andersonfpcorrea@gmail.com>
2024-02-20 21:05:34 -0300

    feat: add content

--- javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
+++ javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
@@ -2,6 +2,7 @@ import Component from "@glimmer/component";
 
 export default class CustomVsatSearchInitialOptions extends Component {
   constructor() {
+    super();
     console.log("Hello, world");
   }
 }
