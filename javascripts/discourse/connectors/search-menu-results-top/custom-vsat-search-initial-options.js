baa958aeb4a7ddf291c95dddc294f332a249b590
Anderson Correa <andersonfpcorrea@gmail.com>
2024-02-20 21:28:10 -0300

    feat: add content

--- javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
+++ javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
@@ -5,10 +5,10 @@ export default class CustomVsatSearchInitialOptions extends Component {
     super();
     console.log("Hello, world");
     const el = document.querySelector("#custom-vsat-init-options");
-    const labelEl = el.querySelector("span.label-suffix");
-    labelEl.innerText = "in Viasat Help Articles";
-    el.addEventListener("click", () => {
-      const query = el.querySelector("span.search-item-prefix").innerText;
+    const labelEl = el?.querySelector("span.label-suffix");
+    labelEl?.innerText = "in Viasat Help Articles";
+    el?.addEventListener("click", () => {
+      const query = el?.querySelector("span.search-item-prefix").innerText;
       location.href = `https://www.forum.test.viasat.com/docs?search=${query}&tag=article`;
     });
   }
