cc0a4d01ca0b51d8c9f376568fa5f60d66668f5a
Anderson Correa <andersonfpcorrea@gmail.com>
2024-02-20 21:01:02 -0300

    feat: add content

---
+++ javascripts/discourse/connectors/search-menu-results-top/custom-vsat-search-initial-options.js
@@ -0,0 +1,7 @@
+import Component from "@glimmer/component";
+
+export default class BrandOfficialTopics extends Component {
+  constructor() {
+    console.log({ siteSettings: this.siteSettings });
+  }
+}
